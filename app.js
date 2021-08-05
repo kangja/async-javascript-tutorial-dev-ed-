cc6a924f158345a2c72f7b117ce6a18d0f75d4a6
Jason <kangja1996@gmail.com>
2021-08-05 09:24:09 -0400

    finished

--- app.js
+++ app.js
@@ -111,3 +111,44 @@
 // });
 
 // Promise.all([yt, fb]).then((result) => console.log(result));
+
+console.log("start");
+
+function loginUser(email, password) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("now we have the data");
+      resolve({ userEmail: email });
+    }, 3000);
+  });
+}
+
+function getUserVideos(email) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(["video1", "video2", "video3"]);
+    }, 2000);
+  });
+}
+
+function videoDetails(video) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("title of the video");
+    }, 2000);
+  });
+}
+
+// loginUser("jkang@gmail.com", "hello")
+//   .then((user) => getUserVideos(user.email))
+//   .then((videos) => videoDetails(videos[0]))
+//   .then((detail) => console.log(detail));
+
+async function displayUser() {
+  const loggedUser = await loginUser("jason", 123456);
+  const videos = await getUserVideos(loggedUser.userEmail);
+  const detail = await videoDetails(videos[0]);
+  console.log(detail);
+}
+
+displayUser();
